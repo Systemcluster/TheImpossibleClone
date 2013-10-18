067e8d5316b5b3316f6cf9bfd2864cb0615fa2c0
Lazy-Senior <boening.v@gmail.com>
2013-10-18 13:30:15 +0200

    Update jump mechanic

--- src/actors/Player.java
+++ src/actors/Player.java
@@ -9,7 +9,17 @@ import core.Scene;
 
 public class Player extends Actor {
 	
-	private double weight = 0.0015;
+	//TODO : CHANGE VALUES
+	private double msAirMax = 0;
+	private double maxHeight = 0;
+	//-- TODO
+	
+	private double msAirStart = 0;
+	private double msAir = 0;
+	private double initY = 0;
+	
+	//w = 0.0018
+	private double weight = 0.0022;
 	private double force = 0;
 	
 	public Player(Scene parent) {
@@ -36,27 +46,54 @@ public class Player extends Actor {
 		return false;
 	}
 
-	public void addForce(double force) {
-		if(isGrounded() || touchObstacle()) this.force = force;
+	public void addForce(double force, double maxHeight) {
+		if(isGrounded() || touchObstacle()){
+			this.force = force;
+			this.maxHeight = y - maxHeight;
+			this.msAirStart = System.currentTimeMillis();
+			this.initY = y;
+		}
 	}
 	
 	@Override
 	public void update() {
+	
 		x += parent.getScrollSpeed();
-		y += force;
-		force += weight;
 		
-		if(touchObstacle()) force = 0;
-		if(isGrounded()) y = parent.getGround(); 
 	}
 	
 	@Override
-	public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g ) {
+
+		//--JUMP--
+		if(isGrounded() || touchObstacle()){
+			msAir = 0;
+		}
+		else{
+			msAir = System.currentTimeMillis() - msAirStart;
+			 
+		}
+		
+		if(parent.getSpaceState() && y > maxHeight ){
+			force += weight * ((y - initY)/(maxHeight - initY));
+		}
+		else{
+			force += weight;
+		}
+		if(touchObstacle() && force > 0) force = 0;
+		y+=force;
+		
+		if(isGrounded()) y = parent.getGround(); 
+		//--/JUMP--
+		
+		//RENDER
 		Graphics2D g2D = (Graphics2D) g;
 		
 		g2D.setColor(Color.black);
 		//System.out.println(parent.getCoordX(x)+" "+ parent.getCoordY(y)+" "+ parent.getWidth(w)+" "+ parent.getHeight(h)+" - "+parent.getPosition());
 		g2D.drawRect(parent.getCoordX(x), parent.getCoordY(y), parent.getWidth(w), parent.getHeight(h));
+	
+
 	}
 	
 }
