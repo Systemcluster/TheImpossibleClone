aad5e65814fc308ed679a23bb8d9a01dc8d2aa3d
lazy-senior <lazy-senior@users.noreply.github.com>
2014-02-05 16:58:42 +0100

    Update AudioIO.java

--- src/sound/AudioIO.java
+++ src/sound/AudioIO.java
@@ -6,7 +6,15 @@ import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * Provides a method to read an audiofile
+ */
 public class AudioIO {
+	/**
+	 * Read an audiofile
+	 * 
+	 * @param in File to read
+	 */
 	public static AudioClip read (BufferedInputStream in){
 		try{
 			return new AudioClip(in);
